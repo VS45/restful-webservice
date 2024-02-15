8fe0f6a134507b024b80b362e02527ca6d801814
vs45 <bringfirehjake88@gmail.com>
2024-02-15 14:08:20 +0100

    customized exceptions error handling

---
+++ src/main/java/com/vs45tech/com/restfulwebservice/exception/ErrorDetails.java
@@ -0,0 +1,34 @@
+package com.vs45tech.com.restfulwebservice.exception;
+
+//import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class ErrorDetails {
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+   /*  
+    public ErrorDetails() {
+    } */
+    public ErrorDetails(LocalDateTime timestamp, String message, String details) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+   
+    public String getMessage() {
+        return message;
+    }
+  
+    public String getDetails() {
+        return details;
+    }
+    @Override
+    public String toString() {
+        return "ErrorDetails [timestamp=" + timestamp + ", message=" + message + ", details=" + details + "]";
+    }
+    
+}
