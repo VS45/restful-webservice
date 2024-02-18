b158f114af4badfff8ee20c63479d85d9c7da816
vs45 <bringfirehjake88@gmail.com>
2024-02-18 14:26:17 +0100

    securing api

---
+++ src/main/java/com/vs45tech/com/restfulwebservice/security/SpringSecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.vs45tech.com.restfulwebservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+@Configuration
+public class SpringSecurityConfiguration {
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+// all request should be authenticated
+http.authorizeHttpRequests(
+    auth->auth.anyRequest().authenticated()
+);
+http.httpBasic(withDefaults());
+http.csrf().disable();
+        return http.build();
+    }
+}
